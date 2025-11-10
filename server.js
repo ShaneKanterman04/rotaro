db51243839572bb712cf42ed4c12056880be7205
kanterman04 <kanterman04@gmail.com>
2025-11-10 12:11:51 -0500

    update

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 app.use(express.static('public'));
+
+// Serve host page at /host (so requesting /host returns host.html)
+app.get('/host', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'host.html'));
+});
+
+app.get('/client', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'client.html'));
+});
 
 const server = http.createServer(app);
 const io = new Server(server);
